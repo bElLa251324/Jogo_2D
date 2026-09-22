60e1fa4a8aac92962bee7b7b241551e16d5a6776
Gabriel <isabellalysany9@gmail.com>
2026-09-22 09:43:35 -0300

    programação do pulo
    progamação do pulo

--- Assets/script/player.cs
+++ Assets/script/player.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class player : MonoBehaviour
@@ -6,21 +7,39 @@ public class player : MonoBehaviour
 
     private Rigidbody2D rb;
 
-
+    private bool isGrounded = false;
+   
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();  
+
     }
 
     
     void Update()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal"); // vai fazer ele andar para frente e para atrás
+        float moveHorizontal = Input.GetAxis("Horizontal"); // vai fazer reconhecer o movimento horizontal
 
         rb.linearVelocity = new Vector2 (moveHorizontal * speed, rb.linearVelocity.y);
-       if (Input.GetKeyDown(KeyCode.Space))
+
+       if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             rb.AddForce(new Vector2(0f, 5f), ForceMode2D.Impulse); // vai servir para pular
+        }
+    }
+
+    void OnCollisionEnter2D(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true; // vai reconhecer quando o jogador estiver no chão
+        } 
+    }
+   void OnCollisionExit2D (Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false; // vai reconhecer quando o jogador pular e ficar no ar
         }
     }
 }
